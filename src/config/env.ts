6fd07cff083fef6b71dc7a66f79895181c26012f
adnanbinakhlas <adnanbinakhlas.dev@gmail.com>
2026-07-20 03:36:56 +0600

    chore: add env specific path

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 export const env = {
   PORT: process.env.PORT as string,
