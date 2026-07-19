da6277ce40966405bd195fc4723d637e73da0cb7
adnanbinakhlas <adnanbinakhlas.dev@gmail.com>
2026-07-20 03:56:17 +0600

    chore: mongodb setup via docker compose

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,5 @@ dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 export const env = {
   PORT: process.env.PORT as string,
   NODE_ENV: process.env.NODE_ENV as string,
+  DATABASE_URL: process.env.DATABASE_URL as string,
 };
