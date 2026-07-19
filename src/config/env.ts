803deacf216fa2511b79dd99c5a045e76e0ac550
adnanbinakhlas <adnanbinakhlas.dev@gmail.com>
2026-07-20 04:18:30 +0600

    chore: add clerk & imagekit env

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,4 +7,7 @@ export const env = {
   PORT: process.env.PORT as string,
   NODE_ENV: process.env.NODE_ENV as string,
   DATABASE_URL: process.env.DATABASE_URL as string,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY as string,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY as string,
+  IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY as string,
 };
